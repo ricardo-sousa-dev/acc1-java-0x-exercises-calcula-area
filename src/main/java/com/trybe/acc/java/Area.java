b9449b85d09bed179dbdca3559f1ff651ddaa3af
ricardo-sousa <rwmsousa@gmail.com>
2022-03-17 13:02:45 -0300

    formatt code

--- src/main/java/com/trybe/acc/java/Area.java
+++ src/main/java/com/trybe/acc/java/Area.java
@@ -7,15 +7,15 @@ public class Area {
    /**
     * Método inicial.
     */
-   public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
-      System.out.println("Digite a medida do lado do quadrado:");
-      double lado = scan.nextDouble();
-      double area = lado * lado;
+        System.out.println("Digite a medida do lado do quadrado:");
+        double lado = scan.nextDouble();
+        double area = lado * lado;
 
-      System.out.println("A area do quadrado de lado " + lado + " é igual a " + area);
+        System.out.println("A area do quadrado de lado " + lado + " é igual a " + area);
 
-      scan.close();
-   }
+        scan.close();
+      }
 }
