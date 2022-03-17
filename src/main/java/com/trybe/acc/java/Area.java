437308539de56fef2dafa17dc856ccfd56cc95be
ricardo-sousa <rwmsousa@gmail.com>
2022-03-17 10:18:55 -0300

    fix: formmat code

--- src/main/java/com/trybe/acc/java/Area.java
+++ src/main/java/com/trybe/acc/java/Area.java
@@ -4,21 +4,20 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Area {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
-      // Ler a entrada de um número pelo console
-      Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        // Ler a entrada de um número pelo console
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
-      System.out.println("Digite a medida do lado do quadrado:");
-      // lê um double:
-      // https://www.devmedia.com.br/como-funciona-a-classe-scanner-do-java/28448
-      double lado = scan.nextDouble();
-      double area = lado * lado;
+        System.out.println("Digite a medida do lado do quadrado:");
+        // lê um double:
+        // https://www.devmedia.com.br/como-funciona-a-classe-scanner-do-java/28448
+        double lado = scan.nextDouble();
+        double area = lado * lado;
 
-      System.out.println("A area do quadrado de lado " + lado + " é igual a " +
-            area);
+        System.out.println("A area do quadrado de lado " + lado + " é igual a " + area);
 
-      scan.close();
-      // https://stackoverflow.com/questions/12519335/resource-leak-in-is-never-closed
+        scan.close();
+        // https://stackoverflow.com/questions/12519335/resource-leak-in-is-never-closed
    }
 }
