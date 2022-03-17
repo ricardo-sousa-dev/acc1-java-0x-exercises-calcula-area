e0034ef0a997327598ac6f9333530329362bcea4
ricardo-sousa <rwmsousa@gmail.com>
2022-03-17 10:31:00 -0300

    formatt code

--- src/main/java/com/trybe/acc/java/Area.java
+++ src/main/java/com/trybe/acc/java/Area.java
@@ -6,18 +6,17 @@ import java.util.Scanner;
 public class Area {
   public static void main(String[] args) {
 
-    // Ler a entrada de um número pelo console
+    /** lê a entrada de um número pelo console */ 
     Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
     System.out.println("Digite a medida do lado do quadrado:");
-    // lê um double:
-    // https://www.devmedia.com.br/como-funciona-a-classe-scanner-do-java/28448
+    /** lê um double: https://www.devmedia.com.br/como-funciona-a-classe-scanner-do-java/28448`*/
     double lado = scan.nextDouble();
     double area = lado * lado;
 
     System.out.println("A area do quadrado de lado " + lado + " é igual a " + area);
 
     scan.close();
-   // https://stackoverflow.com/questions/12519335/resource-leak-in-is-never-closed
+   /** https://stackoverflow.com/questions/12519335/resource-leak-in-is-never-closed */
    }
 }
